e3d900409375ac35cf460c63bbb8470665754e0f
Mehak <52915627+Mehakkhan98@users.noreply.github.com>
2022-02-08 00:24:39 +0500

    Project architechure

---
+++ RestaurantManagement.Api/Controllers/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestaurantManagement.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController
+    {
+        private static readonly string[] Food = new[]
+      {
+            "Pizza", "Burger", "Shawarma", "Sandwich", "French Fries",
+        };
+        private static readonly int[] Price = new[]
+     {
+            1100, 400, 300, 500, 250,
+        };
+        private readonly ILogger<MenuController> _logger;
+        public MenuController(ILogger<MenuController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<MenuModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MenuModel
+            {
+                
+                Price =Price[rng.Next(Price.Length)],
+                Menu = Food[rng.Next(Food.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
+
